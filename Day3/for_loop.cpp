c9a08dc9abf8e9b8742c4c41b4fd67c350209890
pikarabbani10 <rabbanihosni@gmail.com>
2024-10-19 04:15:05 +0600

    stake2

---
+++ Day3/for_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // for (int i=1;i<=5;i++){}
+    for (int i=50;i>=10;i-=5){
+
+        cout << i <<'.'<< " " << "Hello\n";
+    
+    }
+    return 0;
+}
+// 3 TYPES OF LOOPS---->
+//FOR LOOP
+//wHILE LOOP
+//DO WHILE LOOP
